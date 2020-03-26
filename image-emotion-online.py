a616b8d3eb071ae7c9a61f5815feb959a146e6c6
Shiki Takeuchi <takeuchi@ii.ist.i.kyoto-u.ac.jp>
2020-03-26 14:15:19 +0900

    change path "resource"

--- image-emotion-online.py
+++ image-emotion-online.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.4),
-    on Thu Mar 26 14:14:42 2020
+    on Thu Mar 26 14:14:36 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -46,7 +46,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='/Users/takeuchi/research/image-sentiment/gaze-and-emotion/online-experiment/image-emotion-online_lastrun.py',
+    originPath='/Users/takeuchi/research/image-sentiment/gaze-and-emotion/online-experiment/image-emotion-online.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
